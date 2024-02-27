5babda2c05514932986e226a719050e5f0ddadbb
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-02-27 20:22:24 -0300

    First Commit

---
+++ src/main/kotlin/com/lucas/learn/avergersapi/AvergersApiApplication.kt
@@ -0,0 +1,11 @@
+package com.lucas.learn.avergersapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvergersApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvergersApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/lucas/learn/avergersapi/AvergersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lucas.learn.avergersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvergersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
