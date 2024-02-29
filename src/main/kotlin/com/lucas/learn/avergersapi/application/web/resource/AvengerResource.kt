92e154a1eeec46256e44454ccec33131099d802f
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-02-28 21:17:50 -0300

    feat: create DELETE Avenger endpoint

--- src/main/kotlin/com/lucas/learn/avergersapi/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/com/lucas/learn/avergersapi/application/web/resource/AvengerResource.kt
@@ -6,6 +6,7 @@ import com.lucas.learn.avergersapi.domain.avenger.AvengerRepository
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -53,4 +54,8 @@ class AvengerResource(
             ResponseEntity.accepted().body(AvengerResponse.from(avenger))
         }
     } ?: ResponseEntity.notFound().build<Void>()
+
+    @DeleteMapping("/{id}")
+    fun deleteAvenger(@PathVariable("id") id: Long) =
+        repository.delete(id).let { ResponseEntity.accepted().build<Void>() }
 }
